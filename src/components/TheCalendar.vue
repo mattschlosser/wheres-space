9eb008be4c3df31d6e7655c219dc56b977a7d930
Matt Schlosser <matt.schlosser@ualberta.ca>
2020-01-18 20:48:14 -0700

    Extended hours for calendar

--- src/components/TheCalendar.vue
+++ src/components/TheCalendar.vue
@@ -1,7 +1,7 @@
 <template>
   <table class='table table-striped table-sm table-responsive'>
-    <thead><th style='position: sticky;'>Room </th><th v-for="i in Array(22).fill(0).map((e,i) => i+16)" :key="i"> {{ hour(i) }}</th></thead>
-    <tr v-for="room in rooms" :key="room.name"><th scope='row' style='height: 10px; white-space:nowrap; position: sticky;'>{{room.name}}</th><td v-for="i in Array(22).fill(0).map((e,i) => i+16)" :key="i" :class="{booked: is_booked(room, i)}" >&nbsp;</td></tr>
+    <thead><th style='position: sticky;'>Room </th><th v-for="i in Array(27).fill(0).map((e,i) => i+15)" :key="i"> {{ hour(i) }}</th></thead>
+    <tr v-for="room in rooms" :key="room.name"><th scope='row' style='height: 10px; white-space:nowrap; position: sticky;'>{{room.name}}</th><td v-for="i in Array(27).fill(0).map((e,i) => i+15)" :key="i" :class="{booked: is_booked(room, i)}" >&nbsp;</td></tr>
   </table>
 </template>
 <script>
